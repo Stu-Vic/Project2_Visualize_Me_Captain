e7e31612a9596481c0a8515e3de51941a5c41cca
Hamish Cocks <hpcoc2@gmail.com>
2021-06-09 23:09:47 +1000

    endpoint things

--- app/templates/static/js/logic.js
+++ app/templates/static/js/logic.js
@@ -1,18 +1,53 @@
 //this is the logic script for any d3 stuff we want to use
-function optionChanged(incomingData) {
 
-    // wordCloud(incomingData);
-    // Arc(incomingData);
-    dashBoard(incomingData);
+
+init();
+
+function init() {
+
+    d3.json(`"https://twitterusers.herokuapp.com/api/names`).then((data) => {
+
+        var data = data
+
+        names  = data.(grab names from the json data)
+        // appending the names to the dropdown list
+        names.forEach(function(Append) {
+            var option = d3.select("#selDataset");
+            option.append("option").text(Append);
+        });
+
     
+
+    })
+}
+
+
+
+////// 
+
+
+/api/Akshay%20Kumar
+
+
+
+//////this is to run after the json is exposed by the API
+
+function optionChanged(value) {
+ // call the API with the name selected, so call ..../Katy Perry from the database
+    wordCloud(value);
+    Arc(value);
+    dashBoard(value)
+        
 };
 
-function wordCloud() {
+function wordCloud(data) {
 
-    d3.json(import the json API file here).then((data) => {
+    var user_imput = data
+
+    d3.json(`"https://twitterusers.herokuapp.com/api/wordcloud/?identity=" + ${user_input}`).then((data) => {
         //getting the selected value 
 
-        var sel_value = parseString(d3.select("#selDataset").property("value"));
+        //var sel_value = parseString(d3.select("#selDataset").property("value"));
 
 
     });
@@ -22,7 +57,7 @@ function Arc() {
 
     d3.json(import API endpoint).then((data) => {
 
-        var sel_value = parseString(d3.select("#selDataset").property("value"));
+        //var sel_value = parseString(d3.select("#selDataset").property("value"));
 
     })
 }
@@ -40,6 +75,7 @@ function dashBoard() {
             option.append("option").text(Append);
         });
 
+    
 
     })
 }
